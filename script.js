9f2a3e136451d32a2791f284057e30c91916ce9a
BlarQ <collinsogunlala@gmail.com>
2024-04-28 22:48:10 +0100

    feat: tailwindcss design for clock app

---
+++ script.js
@@ -0,0 +1,5 @@
+const hour = document.getElementById('hour')
+const min = document.getElementById('min')
+const sec = document.getElementById('sec')
+const mil = document.getElementById('mil')
+const daylight = document.getElementById('daylight')
